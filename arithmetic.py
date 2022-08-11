9ad50c3f3173339ff41519693228621537ef680d
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-08-11 13:45:12 +0530

    operators
    python

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+#operators
+#arithmetic operator
+'''+ plus operator
+- minus operator
+* mul operator
+% per operator
+/ div operator'''
+
+sasi=2000
+suji=6000
+total_salary=sasi+suji
+print("my salary is:",total_salary)
+
+sasi=2000
+suji=6000
+my_salary=sasi-suji
+print("minus operator",my_salary)
+
+sasi=2000
+suji=6000
+total_salary=sasi*suji
+print("mul operator",total_salary)
+
+sasi=2000
+suji=6000
+total_salary=sasi/suji
+print("div operator",total_salary)
+
+sasi=2000
+suji=6000
+total_salary=sasi%suji
+print("per operator",total_salary)
