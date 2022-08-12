bc8171842bab7957ae965b3bc934484acd7adf58
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-08-12 14:26:48 +0530

    calculation
    python

---
+++ string.py
@@ -0,0 +1,50 @@
+#data type
+#integer
+#float
+#string
+#boolean
+'''a=2672345678
+print(type(a))
+b=34.6
+print(type(b))
+c="hello@#$%^&"
+print(type(c))
+d=True#1 false 0
+print(type(d))'''
+
+#run time
+
+'''sasi=int(input("enter the sasi salary:"))
+suji=int(input("enter the suji salary"))
+print("plus operator",sasi+suji)
+print("minus operator",sasi-suji)
+print("mul operator",sasi*suji)
+print("div operator",sasi/suji)
+print("per operator",sasi%suji)'''
+
+#cent calculation
+'''cent=float(input("enter the cent value:"))
+acre=cent/100
+print("cent value is",cent,"acre value is",acre)'''
+
+#amount calculation
+'''
+deposite=int(input("enter the amount:"))
+intrest=6.7
+profit=(deposite*intrest)/100
+maturity=deposite+profit
+print("yearly intrest",profit)
+print("yearly total amount",maturity)
+print("yearly twise",profit/2)
+print("yearly four times",profit/4)'''
+
+alphabets="abcdefghijklmnopqrstuvwxyz"
+print(alphabets.upper())
+sasi="HELLO"
+print(sasi.lower())
+print(alphabets)
+print(alphabets[0:9])
+print(alphabets[8:12])
+print(alphabets[25])
+print(alphabets[-1])
+print(alphabets[-3])
